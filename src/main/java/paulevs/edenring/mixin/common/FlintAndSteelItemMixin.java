695f2adc749c06eebcd93a90ac27c43fff5eef5a
paulevsGitch <paulevs@yandex.ru>
2021-11-18 09:54:08 +0300

    Fixed possibility to open portal in no-overworld dimensions

--- src/main/java/paulevs/edenring/mixin/common/FlintAndSteelItemMixin.java
+++ src/main/java/paulevs/edenring/mixin/common/FlintAndSteelItemMixin.java
@@ -2,6 +2,7 @@ package paulevs.edenring.mixin.common;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.FlintAndSteelItem;
 import net.minecraft.world.item.context.UseOnContext;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import paulevs.edenring.EdenRing;
 import paulevs.edenring.world.EdenPortal;
 
 @Mixin(FlintAndSteelItem.class)
@@ -20,12 +22,16 @@ public class FlintAndSteelItemMixin {
 		if (useOnContext.getClickedFace() == Direction.UP) {
 			BlockPos pos = useOnContext.getClickedPos();
 			Level level = useOnContext.getLevel();
-			if (level.getBlockState(pos).is(Blocks.GOLD_BLOCK)) {
+			if (correctDimension(level.dimension()) && level.getBlockState(pos).is(Blocks.GOLD_BLOCK)) {
 				if (EdenPortal.checkNewPortal(level, pos.above())) {
 					info.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide()));
 					EdenPortal.buildPortal(level, pos.above());
 				}
 			}
 		}
+	}
+	
+	private boolean correctDimension(ResourceKey dimension) {
+		return dimension.equals(Level.OVERWORLD) || dimension.equals(EdenRing.EDEN_RING_KEY);
 	}
 }

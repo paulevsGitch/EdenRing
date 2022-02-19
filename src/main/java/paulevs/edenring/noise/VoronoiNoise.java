fe16f158f9bfbc14a057859815fb4fd80c747711
paulevsGitch <paulevs@yandex.ru>
2022-02-19 15:21:37 +0300

    More cave generation

--- src/main/java/paulevs/edenring/noise/VoronoiNoise.java
+++ src/main/java/paulevs/edenring/noise/VoronoiNoise.java
@@ -32,10 +32,63 @@ public class VoronoiNoise {
 		Arrays.sort(buffer);
 	}
 	
+	public void getDistances(int seed, double x, double z, float[] buffer, Random random) {
+		int x1 = Mth.floor(x);
+		int z1 = Mth.floor(z);
+		float sdx = (float) (x - x1);
+		float sdz = (float) (z - z1);
+		
+		byte index = 0;
+		float[] point = new float[2];
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				getPoint(seed, x1 + i, z1 + j, random, point);
+				float dx = point[0] + i - sdx;
+				float dz = point[1] + j - sdz;
+				float distance = (float) Mth.length(dx, dz);
+				buffer[index++] = distance;
+			}
+		}
+		Arrays.sort(buffer);
+	}
+	
+	public float getValue(int seed, double x, double z, Random random) {
+		int x1 = Mth.floor(x);
+		int z1 = Mth.floor(z);
+		float sdx = (float) (x - x1);
+		float sdz = (float) (z - z1);
+		
+		float distance = 10;
+		float[] point = new float[2];
+		int[] pointResult = new int[2];
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				getPoint(seed, x1 + i, z1 + j, random, point);
+				float dx = point[0] + i - sdx;
+				float dz = point[1] + j - sdz;
+				float d = (float) Mth.length(dx, dz);
+				if (d < distance) {
+					distance = d;
+					pointResult[0] = i;
+					pointResult[1] = j;
+				}
+			}
+		}
+		
+		random.setSeed(MHelper.getSeed(seed, x1 + pointResult[0], z1 + pointResult[1]));
+		return random.nextFloat();
+	}
+	
 	private void getPoint(int seed, int x, int y, int z, Random random, float[] point) {
 		random.setSeed(MHelper.getSeed(seed, x, y, z));
 		point[0] = random.nextFloat();
 		point[1] = random.nextFloat();
 		point[2] = random.nextFloat();
+	}
+	
+	private void getPoint(int seed, int x, int z, Random random, float[] point) {
+		random.setSeed(MHelper.getSeed(seed, x, z));
+		point[0] = random.nextFloat();
+		point[1] = random.nextFloat();
 	}
 }
--- src/main/java/paulevs/edenring/world/generator/CaveGenerator.java
+++ src/main/java/paulevs/edenring/world/generator/CaveGenerator.java
@@ -3,16 +3,13 @@ package paulevs.edenring.world.generator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockPos.MutableBlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.Heightmap.Types;
 import paulevs.edenring.noise.InterpolationCell;
 import paulevs.edenring.noise.VoronoiNoise;
-import paulevs.edenring.registries.EdenBlocks;
 import ru.bclib.noise.OpenSimplexNoise;
 import ru.bclib.util.BlocksHelper;
 import ru.bclib.util.MHelper;
@@ -20,25 +17,8 @@ import ru.bclib.util.MHelper;
 import java.util.Random;
 
 public class CaveGenerator {
-	private static final BlockState CAVE_AIR = /*Blocks.GLASS.defaultBlockState();*/Blocks.CAVE_AIR.defaultBlockState();
+	private static final BlockState CAVE_AIR = Blocks.CAVE_AIR.defaultBlockState();
 	private static final VoronoiNoise VORONOI_NOISE = new VoronoiNoise();
-	private static final Block[] TERRAIN_BLOCKS = new Block[] {
-		Blocks.STONE,
-		Blocks.GRANITE,
-		Blocks.ANDESITE,
-		Blocks.DIORITE,
-		Blocks.CALCITE,
-		Blocks.TUFF,
-		Blocks.DEEPSLATE,
-		Blocks.IRON_ORE,
-		Blocks.GOLD_ORE,
-		Blocks.COPPER_ORE,
-		Blocks.RAW_IRON_BLOCK,
-		Blocks.RAW_GOLD_BLOCK,
-		Blocks.RAW_COPPER_BLOCK,
-		Blocks.DIRT,
-		EdenBlocks.EDEN_GRASS_BLOCK
-	};
 	private static OpenSimplexNoise simplexNoise;
 	private static int seed;
 	
@@ -49,32 +29,32 @@ public class CaveGenerator {
 	}
 	
 	public static void carve(ChunkAccess chunkAccess) {
-		//if (true) return;
-		
-		long time = System.currentTimeMillis();
-		
 		int minX = chunkAccess.getPos().getMinBlockX();
 		int minZ = chunkAccess.getPos().getMinBlockZ();
 		int minY = chunkAccess.getMinBuildHeight();
 		int maxY = chunkAccess.getMaxBuildHeight();
 		
-		final float[] buffer = new float[27];
+		final float[] buffer9 = new float[9];
+		final float[] buffer27 = new float[27];
 		
-		Random rand = new Random();
+		Random random = new Random();
+		final int maxCell = (maxY - minY) / 8 + 1;
+		final BlockPos origin = new BlockPos(minX, minY, minZ);
 		TerrainGenerator generator = MultiThreadGenerator.getTerrainGenerator();
-		InterpolationCell cellSparce = new InterpolationCell(generator, 4, (maxY - minY) / 16 + 1, 16, 16, new BlockPos(minX - 16, minY, minZ - 16));
-		InterpolationCell cellTerrain = new InterpolationCell(generator, 3, (maxY - minY) / 8 + 1, 8, 8, new BlockPos(minX, minY, minZ));
-		InterpolationCell cellVoronoi = new InterpolationCell(p -> getTunelNoise(p, buffer, rand), 5, (maxY - minY) / 4 + 1, 4, 4, new BlockPos(minX, minY, minZ));
-		InterpolationCell cellBigCave = new InterpolationCell(p -> getBigCaveNoise(cellSparce, p, 0), 5, (maxY - minY) / 4 + 1, 4, 4, new BlockPos(minX, minY, minZ));
+		InterpolationCell cellSparse = new InterpolationCell(generator, 4, (maxY - minY) / 16 + 1, 16, 16, new BlockPos(minX - 16, minY, minZ - 16));
+		InterpolationCell cellTerrain = new InterpolationCell(generator, 3, maxCell, 8, 8, origin);
+		InterpolationCell cellVoronoi = new InterpolationCell(p -> getTunelNoise(p, buffer27, random), 5, (maxY - minY) / 4 + 1, 4, 4, origin);
+		InterpolationCell cellBigCave = new InterpolationCell(p -> getBigCaveNoise(cellSparse, p), 3, maxCell, 8, 8, origin);
+		InterpolationCell cellPillars = new InterpolationCell(p -> getPillars(p, seed, buffer9, random), 5, (maxY - minY) / 4 + 1, 4, 4, origin);
 		
 		MutableBlockPos pos = new MutableBlockPos();
-		//MutableBlockPos posWorld = new MutableBlockPos();
+		int maxCheck = maxY - 16;
 		
 		for (byte x = 0; x < 16; x++) {
-			//posWorld.setX(minX | x);
+			//int wx = x | minX;
 			pos.setX(x);
 			for (byte z = 0; z < 16; z++) {
-				//posWorld.setZ(minZ | z);
+				//int wz = z | minZ;
 				pos.setZ(z);
 				
 				byte index = 0;
@@ -82,9 +62,21 @@ public class CaveGenerator {
 				
 				int max = chunkAccess.getHeight(Types.WORLD_SURFACE_WG, x, z) - 10;
 				
-				for (int y = minY; y < max; y++) {
-					pos.setY(y);
+				for (short y = (short) minY; y < max; y++) {
+					if (y < maxCheck) {
+						float heightNoise = cellTerrain.get(pos.setY(y + 10), true);
+						if (heightNoise <= 0) {
+							continue;
+						}
+						if (y > 8) {
+							heightNoise = cellTerrain.get(pos.setY(y - 8), true);
+							if (heightNoise <= 0) {
+								continue;
+							}
+						}
+					}
 					
+					pos.setY(y);
 					if (chunkAccess.getBlockState(pos).getBlock() == Blocks.STONE) {
 						float noise = cellVoronoi.get(pos, true);
 						accumulation[index++] = noise;
@@ -102,15 +94,22 @@ public class CaveGenerator {
 						float cellValue = cellTerrain.get(pos, true);
 						noise = -smoothUnion(-noise, cellValue + 0.5F, 1.1F);
 						
+						float bigCave = 0;
 						if (noise < 0.1F) {
-							noise = MHelper.max(noise, cellBigCave.get(pos, true));
+							bigCave = cellBigCave.get(pos, true);
+							noise = -smoothUnion(-noise, -bigCave, 0.1F);
+						}
+						
+						if (noise > -0.1F) {
+							float pillars = cellPillars.get(pos, true);
+							noise = smoothUnion(noise, pillars, 0.1F);
 						}
 						
 						if (noise > 0) {
 							chunkAccess.setBlockState(pos, CAVE_AIR, false);
 							int py = pos.getY();
 							pos.setY(py + 1);
-							if (!chunkAccess.getBlockState(pos).isAir()) {
+							/*if (!chunkAccess.getBlockState(pos).isAir()) {
 								pos.setY(py + 3);
 								if (chunkAccess.getBlockState(pos).isAir()) {
 									for (byte i = 1; i < 3; i++) {
@@ -125,98 +124,17 @@ public class CaveGenerator {
 										chunkAccess.setBlockState(pos, CAVE_AIR, false);
 									}
 								}
-							}
+							}*/
 						}
 					}
 				}
 			}
 		}
-		
-		/*for (int x = 0; x < 16; x++) {
-			posWorld.setX(minX | x);
-			pos.setX(x);
-			for (int z = 0; z < 16; z++) {
-				posWorld.setZ(minZ | z);
-				pos.setZ(z);
-				
-				byte index = 0;
-				float[] accumulation = new float[8];
-				
-				for (int y = minY; y < maxY; y++) {
-					posWorld.setY(y);
-					pos.setY(y - minY);
-					float cellValue = cell.get(posWorld, false);
-					//float cellValue = cell.get(pos, true);
-					if (cellValue > -0.6F) {
-						//if (isTerrain(chunkAccess.getBlockState(pos))) {
-						float noise = cellVoronoi.get(pos, true);//0;//getTunelNoise(posWorld, buffer, rand);
-						accumulation[index++] = noise;
-						if (index >= accumulation.length) {
-							index = 0;
-						}
-						
-						float average = 0;
-						for (byte i = 0; i < accumulation.length; i++) {
-							noise = MHelper.max(noise, accumulation[i]);
-							average += accumulation[i];
-						}
-						noise = (noise + (average / accumulation.length)) * 0.5F - 0.9F;
-						noise = -smoothUnion(-noise, cellValue + 0.5F, 1.1F);
-						
-						if (noise < 0.1F) {
-							float bigCave = getBigCaveNoise(cell, posWorld, cellValue);
-							noise = MHelper.max(noise, bigCave);
-						}
-						
-						//noise = bigCave;
-						//noise = -smoothUnion(-noise, -bigCave, 1.2F);
-						
-						if (noise > 0) {
-							chunkAccess.setBlockState(pos, CAVE_AIR, false);
-							int py = pos.getY();
-							pos.setY(py + 1);
-							if (!chunkAccess.getBlockState(pos).isAir()) {
-								pos.setY(py + 3);
-								if (chunkAccess.getBlockState(pos).isAir()) {
-									for (byte i = 1; i < 3; i++) {
-										pos.setY(py + i);
-										chunkAccess.setBlockState(pos, CAVE_AIR, false);
-									}
-								}
-								else {
-									pos.setY(py + 4);
-									if (chunkAccess.getBlockState(pos).isAir()) {
-										pos.setY(py + 3);
-										chunkAccess.setBlockState(pos, CAVE_AIR, false);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}*/
-		
-		time = System.currentTimeMillis() - time;
-		System.out.println(time);
 	}
 	
 	private static float getTunelNoise(BlockPos pos, float[] buffer, Random random) {
 		VORONOI_NOISE.getDistances(seed, pos.getX() * 0.01, pos.getY() * 0.03, pos.getZ() * 0.01, buffer, random);
 		return buffer[0] / buffer[2];
-		//return 0;
-	}
-	
-	private static boolean isTerrain(BlockState state) {
-		if (state.is(BlockTags.STONE_ORE_REPLACEABLES) || state.is(BlockTags.DEEPSLATE_ORE_REPLACEABLES)) {
-			return true;
-		}
-		for (Block terrain: TERRAIN_BLOCKS) {
-			if (state.is(terrain)) {
-				return true;
-			}
-		}
-		return false;
 	}
 	
 	private static float smoothUnion(float a, float b, float radius) {
@@ -224,64 +142,39 @@ public class CaveGenerator {
 		return Mth.lerp(h, b, a) - radius * h * (1F - h);
 	}
 	
-	private static float getBigCaveNoise(InterpolationCell cell, BlockPos pos, float cellValue) {
-		//return cellValue - 0.03F;// + 0.1F;
-		/*if (cellValue < 0.1F) {
-			return 0;
+	private static float getMinValue(InterpolationCell cell, BlockPos pos) {
+		float value = 1;
+		for (Direction dir : BlocksHelper.DIRECTIONS) {
+			float side = cell.get(pos.relative(dir, 15), false);
+			value = MHelper.min(value, side);
 		}
-		return 1.0F;*/
-		//float value = (float) simplexNoise.eval(pos.getX() * 0.01, pos.getY() * 0.01, pos.getZ() * 0.01);
-		//value += (float) simplexNoise.eval(pos.getX() * 0.07, pos.getY() * 0.07, pos.getZ() * 0.07) * 0.3F;
-		//return Mth.clamp(value, 0, 1) * (cellValue - 0.3F) / 0.7F;
-		
+		return value;
+	}
+	
+	private static float getBigCaveNoise(InterpolationCell cell, BlockPos pos) {
 		if (pos.getY() < 32 || pos.getY() > 224) {
 			return 0;
 		}
 		
-		/*float noiseX = (float) simplexNoise.eval(pos.getY() * 0.1, pos.getZ() * 0.1) * 3F + 0.5F;
-		float noiseY = (float) simplexNoise.eval(pos.getX() * 0.1, pos.getZ() * 0.1) * 3F + 0.5F;
-		float noiseZ = (float) simplexNoise.eval(pos.getX() * 0.1, pos.getY() * 0.1) * 3F + 0.5F;
-		pos = pos.offset(noiseX, noiseY, noiseZ);*/
+		float noise = (float) simplexNoise.eval(pos.getX() * 0.03, pos.getY() * 0.03, pos.getZ() * 0.03);
 		
-		float value = cell.get(pos, false) - 0.03F;
-		/*if (value < 0.02F) {
-			return 0;
-		}*/
-		
-		/*for (Direction dir: BlocksHelper.DIRECTIONS) {
-			if (cell.get(pos.relative(dir, 15), false) < 0) {
-				value = 0;
-				break;
-			}
-		}*/
-		
-		/*for (Direction dir: BlocksHelper.DIRECTIONS) {
-			if (cell.get(pos.relative(dir, 10), false) < 0) {
-				value = 0;
-				break;
-			}
-		}*/
-		
-		float noise = (float) simplexNoise.eval(pos.getX() * 0.01, pos.getY() * 0.01, pos.getZ() * 0.01);
-		//noise = Mth.clamp(noise * 5, 0, 1);
-		//value = -smoothUnion(-value, -noise, 1);
-		//value -= Mth.clamp(noise * 2, 0, 1);
-		value -= Mth.clamp(noise * 0.5F, 0, 1);
-		//value *= noise;
-		
-		noise = (float) simplexNoise.eval(pos.getX() * 0.1, pos.getY() * 0.1, pos.getZ() * 0.1);
-		value += noise * 0.01F;
-		
-		if (value > 0) {
-			for (Direction dir : BlocksHelper.DIRECTIONS) {
-				if (cell.get(pos.relative(dir, 7), false) < 0) {
-					value -= 1;
-					break;
-				}
-			}
+		float value = getMinValue(cell, pos);
+		value = MHelper.max(value, getMinValue(cell, pos.above(8)));
+		value = MHelper.max(value, getMinValue(cell, pos.below(8)));
+		if (noise < 0) {
+			value += noise;
 		}
 		
-		//value = Mth.clamp(value, 0, 1);
-		return value;
+		noise = (float) simplexNoise.eval(pos.getX() * 0.1, pos.getY() * 0.1, pos.getZ() * 0.1) * 0.004F + 0.004F;
+		noise += (float) simplexNoise.eval(pos.getX() * 0.03, pos.getY() * 0.03, pos.getZ() * 0.03) * 0.01F + 0.01F;
+		
+		return value - noise;
+	}
+	
+	private static float getPillars(BlockPos pos, int seed, float[] buffer, Random random) {
+		VORONOI_NOISE.getDistances(seed, pos.getX() * 0.02, pos.getZ() * 0.02, buffer, random);
+		float value = VORONOI_NOISE.getValue(seed, pos.getX() * 0.02, pos.getZ() * 0.02, random);
+		value = buffer[0] - 0.07F * (value * 0.5F + 0.5F);
+		return value + (float) simplexNoise.eval(pos.getX() * 0.03, pos.getY() * 0.03, pos.getZ() * 0.03) * 0.01F;
 	}
 }

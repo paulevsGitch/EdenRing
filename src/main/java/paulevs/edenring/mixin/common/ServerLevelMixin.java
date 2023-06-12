e3f91dd6fb591ac9c070d1c3f3f159a2602389f2
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2023-06-12 11:11:05 -0400

    Fix server mixin

--- src/main/java/paulevs/edenring/mixin/common/ServerLevelMixin.java
+++ src/main/java/paulevs/edenring/mixin/common/ServerLevelMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.world.RandomSequences;
 import net.minecraft.world.level.CustomSpawner;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.BiomeSource;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.ServerLevelData;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,7 +29,21 @@ import java.util.concurrent.Executor;
 @Mixin(ServerLevel.class)
 public class ServerLevelMixin {
 	@Inject(method = "<init>*", at = @At("TAIL"))
-	private void eden_onServerInit(MinecraftServer minecraftServer, Executor executor, LevelStorageSource.LevelStorageAccess levelStorageAccess, ServerLevelData serverLevelData, ResourceKey<Level> resourceKey, LevelStem levelStem, ChunkProgressListener chunkProgressListener, boolean bl, long seed, List<CustomSpawner> list, boolean bl2, CallbackInfo info) {
+	private void eden_onServerInit(
+			MinecraftServer minecraftServer,
+			Executor executor,
+			LevelStorageSource.LevelStorageAccess levelStorageAccess,
+			ServerLevelData serverLevelData,
+			ResourceKey<Level> resourceKey,
+			LevelStem levelStem,
+			ChunkProgressListener chunkProgressListener,
+			boolean bl,
+			long seed,
+			List<CustomSpawner> list,
+			boolean bl2,
+			@Nullable RandomSequences randomSequences,
+			CallbackInfo ci
+	) {
 		ServerLevel level = ServerLevel.class.cast(this);
 		
 		ChunkGenerator chunkGenerator = levelStem.generator();
